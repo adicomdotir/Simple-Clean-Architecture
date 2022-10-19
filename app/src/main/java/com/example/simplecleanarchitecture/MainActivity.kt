ed4c9d6ee320c2b703e542769ea7b491d96f8109
adicomdotir <adicom.ir@gmail.com>
2022-10-20 01:08:27 +0330

    Add view model and complete main activity

--- app/src/main/java/com/example/simplecleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/simplecleanarchitecture/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.simplecleanarchitecture
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import com.example.simplecleanarchitecture.databinding.ActivityMainBinding
 import com.example.simplecleanarchitecture.ui.viewmodel.QuoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,5 +21,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         quoteViewModel.onCreate()
+
+        quoteViewModel.quoteModel.observe(this, Observer {
+            binding.tvQuote.text = it.quote
+            binding.tvAuthor.text = it.author
+        })
+
+        quoteViewModel.isLoading.observe(this, Observer {
+            binding.loading.isVisible = it
+        })
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
     }
 }
--- app/src/main/java/com/example/simplecleanarchitecture/ui/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/simplecleanarchitecture/ui/viewmodel/QuoteViewModel.kt
@@ -2,13 +2,42 @@ package com.example.simplecleanarchitecture.ui.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simplecleanarchitecture.domain.GetQuotesUseCase
+import com.example.simplecleanarchitecture.domain.GetRandomQuoteUseCase
 import com.example.simplecleanarchitecture.domain.model.Quote
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class QuoteViewModel : ViewModel() {
+@HiltViewModel
+class QuoteViewModel @Inject constructor(
+    private val getQuotesUseCase: GetQuotesUseCase,
+    private val getRandomQuoteUseCase: GetRandomQuoteUseCase
+) : ViewModel() {
     val quoteModel = MutableLiveData<Quote>()
     val isLoading = MutableLiveData<Boolean>()
 
     fun onCreate() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val result = getQuotesUseCase()
 
+            if (!result.isNullOrEmpty()) {
+                quoteModel.postValue(result[0])
+                isLoading.postValue(false)
+            }
+        }
+    }
+
+    fun randomQuote() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            val quote = getRandomQuoteUseCase()
+            if (quote != null) {
+                quoteModel.value = quote
+            }
+            isLoading.postValue(false)
+        }
     }
 }
